5e1e8fd0d35503d9300ecf6b0d74c08fcd4ab7e8
dimanranawaka <dimanranawaka@gmail.com>
2023-06-24 21:25:38 +0530

    Basic JS - Comparison Operator

--- CSS/JavaScript/src/Basics.js
+++ CSS/JavaScript/src/Basics.js
@@ -10,5 +10,8 @@ let y=3;
 // let isLessThan= x<y;
 // console.log(isLessThan);
 
-let isGreaterThan = x>y;
-console.log(isGreaterThan);
+// let isGreaterThan = x>y;
+// console.log(isGreaterThan);
+
+let isGreaterThanOrEqual = x>=y;
+console.log(isGreaterThanOrEqual);
