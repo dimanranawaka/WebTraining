8e60b1c9f81778c0eac6adc514188f3412143d7b
dimanranawaka <dimanranawaka@gmail.com>
2023-06-24 18:27:33 +0530

    Basic JS

---
+++ CSS/JavaScript/src/Basics.js
@@ -0,0 +1,8 @@
+let address ='Colombo';
+let text  = 'Simple Text!';
+
+console.log(address); // This is Similar to "sout" IN Java
+console.log(text);
+console.log("==========");
+console.log(typeof text); // this prints the data types
+console.log(typeof address);
