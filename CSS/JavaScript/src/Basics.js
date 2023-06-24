52c009ec3a47ab63e1f40b1e220cb0200eeb58fa
dimanranawaka <dimanranawaka@gmail.com>
2023-06-24 21:29:10 +0530

    Basic JS - Logical Operator

--- CSS/JavaScript/src/Basics.js
+++ CSS/JavaScript/src/Basics.js
@@ -1,20 +1,5 @@
 let x=5;
 let y=3;
 
-// let isEqual=x==y;
-// console.log(isEqual);
-
-// let isNotEqual=x!=y;
-// console.log(isNotEqual);
-
-// let isLessThan= x<y;
-// console.log(isLessThan);
-
-// let isGreaterThan = x>y;
-// console.log(isGreaterThan);
-
-// let isGreaterThanOrEqual = x>=y;
-// console.log(isGreaterThanOrEqual);
-
-let isLessThanOrEqual = x<=y;
-console.log(isLessThanOrEqual);
+let answer=(x>0) && (y>0);
+console.log(answer);
