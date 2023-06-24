bb60fbbc94b08c2e965b5413ff3c572dcabebf85
dimanranawaka <dimanranawaka@gmail.com>
2023-06-24 19:32:12 +0530

    Basic JS - Objects

--- CSS/JavaScript/src/Basics.js
+++ CSS/JavaScript/src/Basics.js
@@ -1,4 +1,9 @@
-let values = [1,2,3,4,5];
-let salad = ['text',10,-20,20.5,true];
-console.log(salad);
-console.log(typeof salad);
+let man ={
+    name: 'Diman',
+    age:50,
+    address:'Matara',
+    fav:[10,'dog'],
+    country:{code:94,alpha2:50}
+}
+console.log(man);
+console.log(typeof man);
