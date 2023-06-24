c43fab6e9fc598ae7fbbd87d26cc03db8e9f81b9
dimanranawaka <dimanranawaka@gmail.com>
2023-06-24 18:37:34 +0530

    Basic JS - Boolean

--- CSS/JavaScript/src/Basics.js
+++ CSS/JavaScript/src/Basics.js
@@ -1,17 +1,8 @@
-let age = 40;
-let salary  = 2000000;
-let tax = -200;
-let sampleNumber1=10.5;
-let sampleNumber2=-10.5;
+let isExists = true;
+let saved = false;
 
-console.log(age);
-console.log(salary);
-console.log(tax);
-console.log(sampleNumber1);
-console.log(sampleNumber2);
-console.log("============");
-console.log(typeof age);
-console.log(typeof salary);
-console.log(typeof tax);
-console.log(typeof sampleNumber1);
-console.log(typeof sampleNumber2);
+console.log(isExists);
+console.log(saved);
+console.log("========");
+console.log(typeof isExists);
+console.log(typeof saved);
