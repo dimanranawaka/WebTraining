6eed66c23a29b3eb8cb21e9211005087e6ccf8f0
dimanranawaka <dimanranawaka@gmail.com>
2023-06-24 21:13:31 +0530

    Basic JS - Assignment Operator

--- CSS/JavaScript/src/Basics.js
+++ CSS/JavaScript/src/Basics.js
@@ -1,24 +1,6 @@
 // +,-,*,/,%,++,--
 //========================
-let answer1=10+20;
-console.log(answer1);
-let answer2=10-20;
-console.log(answer2);
-let answer3=10*20;
-console.log(answer3);
-let answer4=10/20;
-console.log(answer4);
-let answer5=10%20;
-console.log(answer5);
-let x=10;
-let answer6=++x; //Pre-Increment
-console.log(answer6);
-let y=10;
-let answer7=y++; //Post-Increment
-console.log(answer7);
-let z=20;
-let answer8=--y;//Pre-Decrement
-console.log(answer8);
-let q=20;
-let answer9=q--;//Post-Decrement
-console.log(answer9);
+let x=5;
+let y=3;
+x+=y;
+console.log(x);
