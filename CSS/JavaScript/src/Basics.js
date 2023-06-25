23a5d1b84dac783524a7dc6b139bf2d05fe6aeea
dimanranawaka <dimanranawaka@gmail.com>
2023-06-25 22:34:25 +0530

    Basic JS - Arrays(concat())

--- CSS/JavaScript/src/Basics.js
+++ CSS/JavaScript/src/Basics.js
@@ -1,14 +1,4 @@
 let cart = ['Apple','Mango','Banana'];
-//push() , unshift();
-cart.unshift('Orange'); // unshift() method add an element as First
-console.log(cart);
-
-//---------Remove-------------
-//splice(start index(required),(delete count(Optional)))
-//shift()
-//pop()
-// cart.splice(1,2);
-// cart.shift();
-let x=cart.pop(); // remove and return element
-console.log(cart);
-console.log(x);
+let animal=['Dog','Cat'];
+let newArr = cart.concat(animal);
+console.log(newArr);
