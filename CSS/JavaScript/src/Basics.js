57b4a7b819b3585928f37ed9c8c0a869020e6574
dimanranawaka <dimanranawaka@gmail.com>
2023-06-25 21:36:41 +0530

    Basic JS - Functions

--- CSS/JavaScript/src/Basics.js
+++ CSS/JavaScript/src/Basics.js
@@ -1,11 +1,11 @@
-let calc=function (num1,num2){ // Expression based Anonymous Functions
-    let total1=num1+num2;
-    console.log(total1);
+function calc (num1,num2){
+    return num1+num2;
+
 }
 let num1=10;
 let num2=20;
-calc(num1,num2);// invoke ,argument
+console.log(calc(num1,num2));
 
 let num3=30;
 let num4=40;
-calc(num3,num4);// invoke ,argument
+console.log(calc(num3,num4));
