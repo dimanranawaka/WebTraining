580431a41ecf2a4717a8ad38adfbeafbe3a03734
dimanranawaka <dimanranawaka@gmail.com>
2023-06-25 23:01:06 +0530

    Basic JS - Arrays(filter())

--- CSS/JavaScript/src/Basics.js
+++ CSS/JavaScript/src/Basics.js
@@ -1,6 +1,6 @@
 let numbers = [1,2,3,4,5,6,7,8,9,10];
 
-let oddNumbers=numbers.filter(function (data){
-    return data % 2 !==0;
-})
+let oddNumbers=numbers.filter(data=>data % 2 !==0);
+
+
 console.log(oddNumbers);
