0a5631d184eac313eb322e7c34b8ca11477f2449
dimanranawaka <dimanranawaka@gmail.com>
2023-06-25 20:05:26 +0530

    Basic JS - Conditional Statements

--- CSS/JavaScript/src/Basics.js
+++ CSS/JavaScript/src/Basics.js
@@ -1,4 +1,6 @@
-let x = 10;
-let y ='10';
-console.log(x==y); //Checks only the Value
-console.log(x===y); // Check both value and Data type
+let age=20;
+if(age>=18){
+    console.log('Eligible for this movies');
+}else{
+    console.log("To Small For the movies");
+}
