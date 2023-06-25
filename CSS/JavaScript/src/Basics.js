bfd52315662f5e2cb644079bd730a1b793529310
dimanranawaka <dimanranawaka@gmail.com>
2023-06-25 22:03:05 +0530

    Basic JS - Basic Array

--- CSS/JavaScript/src/Basics.js
+++ CSS/JavaScript/src/Basics.js
@@ -2,3 +2,11 @@ let cart = ['Apple','Mango','Banana'];
 console.log(cart);
 console.log(cart[0]);
 console.log(cart.length);
+//-------------------
+
+const customerData=[
+    {id:1,name:'Diman',city:'Colombo'},
+    {id:2,name:'Dasun',city:'Matara'},
+    {id:3,name:'Ranawaka',city:'Galle'},
+];
+console.table(customerData);
