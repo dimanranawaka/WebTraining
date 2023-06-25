28917979348e61413abd6e0c74f5228b0b86bacd
dimanranawaka <dimanranawaka@gmail.com>
2023-06-25 20:27:07 +0530

    Basic JS - While Loop

--- CSS/JavaScript/src/Basics.js
+++ CSS/JavaScript/src/Basics.js
@@ -1,12 +1,5 @@
-let day = "Sunday";
-switch (day) {
-    case "Monday": console.log("Monday");
-    break;
-    case "Tuesday": console.log("Tuesday");
-    break;
-    case "Wednesday": console.log("Wednesday");
-    break;
-    case "Sunday":console.log("Sunday");
-    break;
-    default:console.log("Wrong Input");
+let counter = 0;
+while(counter<10){
+    console.log("Hello : "+(counter+1));
+    counter++;
 }
