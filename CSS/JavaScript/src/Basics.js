739eadf98e26932b488b63707794cf71e8900a61
dimanranawaka <dimanranawaka@gmail.com>
2023-06-25 20:20:04 +0530

    Basic JS - Switch

--- CSS/JavaScript/src/Basics.js
+++ CSS/JavaScript/src/Basics.js
@@ -1,12 +1,12 @@
-let marks=90;
-if(marks>=75){
-    console.log('A');
-}else if(marks>=65){
-    console.log("B");
-}else if(marks>=55){
-    console.log("C");
-}else if(marks>=35){
-    console.log("S");
-}else {
-    console.log("Fail");
+let day = "Sunday";
+switch (day) {
+    case "Monday": console.log("Monday");
+    break;
+    case "Tuesday": console.log("Tuesday");
+    break;
+    case "Wednesday": console.log("Wednesday");
+    break;
+    case "Sunday":console.log("Sunday");
+    break;
+    default:console.log("Wrong Input");
 }
