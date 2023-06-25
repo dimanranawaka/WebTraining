4ca31cda044631c161ceee4b1c9849bb4abafda4
dimanranawaka <dimanranawaka@gmail.com>
2023-06-25 22:40:24 +0530

    Basic JS - Arrays(forEach())

--- CSS/JavaScript/src/Basics.js
+++ CSS/JavaScript/src/Basics.js
@@ -1,6 +1,10 @@
 let cart = ['Apple','Mango','Banana'];
 let animal=['Dog','Cat'];
 
-cart.forEach(function (data){
+// cart.forEach(function (data){
+//     console.log(data);
+// });
+
+cart.forEach((data)=>{
     console.log(data);
 });
