edb54fe686a85d5e63a2debf2b338698a9b3b3cd
dimanranawaka <dimanranawaka@gmail.com>
2023-06-25 21:23:10 +0530

    Basic JS - Functions

--- CSS/JavaScript/src/Basics.js
+++ CSS/JavaScript/src/Basics.js
@@ -1,9 +1,11 @@
-// break , continue , (return)
-// for (let i = 0; i < 10; i++) {
-//     console.log(i);
-//     if(i===5){break;}
-// }
-for (let i = 0; i <10; i++) {
-    if(i===5){continue;}
-    console.log(i);
+function calc(num1,num2){
+    let total1=num1+num2;
+    console.log(total1);
 }
+let num1=10;
+let num2=20;
+calc(num1,num2);// invoke ,argument
+
+let num3=30;
+let num4=40;
+calc(num3,num4);// invoke ,argument
