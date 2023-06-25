0dbb9c4b212f7dabf17caf27d96d6ac0af1a5e98
dimanranawaka <dimanranawaka@gmail.com>
2023-06-25 21:45:28 +0530

    Basic JS - Basic Array

--- CSS/JavaScript/src/Basics.js
+++ CSS/JavaScript/src/Basics.js
@@ -1,11 +1,4 @@
-function calc (num1,num2){
-    return num1+num2;
-
-}
-let num1=10;
-let num2=20;
-console.log(calc(num1,num2));
-
-let num3=30;
-let num4=40;
-console.log(calc(num3,num4));
+let cart = ['Apple','Mango','Banana'];
+console.log(cart);
+console.log(cart[0]);
+console.log(cart.length);
