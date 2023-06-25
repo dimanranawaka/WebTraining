53f4426f317ecf4626cd6cd79dd04fe6ddb9c6f5
dimanranawaka <dimanranawaka@gmail.com>
2023-06-25 22:14:31 +0530

    Basic JS - Remove(splice)

--- CSS/JavaScript/src/Basics.js
+++ CSS/JavaScript/src/Basics.js
@@ -2,3 +2,10 @@ let cart = ['Apple','Mango','Banana'];
 //push() , unshift();
 cart.unshift('Orange'); // unshift() method add an element as First
 console.log(cart);
+
+//---------Remove-------------
+//splice(start index(required),(delete count(Optional)))
+//shift()
+//pop()
+cart.splice(1,2);
+console.log(cart);
