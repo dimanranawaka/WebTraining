38d6046babf49a7f27c6e86f1ac7c38f4b19e9f9
dimanranawaka <dimanranawaka@gmail.com>
2023-06-25 22:07:05 +0530

    Basic JS - push();

--- CSS/JavaScript/src/Basics.js
+++ CSS/JavaScript/src/Basics.js
@@ -1,12 +1,4 @@
 let cart = ['Apple','Mango','Banana'];
+//push() , unshift();
+cart.push('Orange'); // Push method add an element as Last
 console.log(cart);
-console.log(cart[0]);
-console.log(cart.length);
-//-------------------
-
-const customerData=[
-    {id:1,name:'Diman',city:'Colombo'},
-    {id:2,name:'Dasun',city:'Matara'},
-    {id:3,name:'Ranawaka',city:'Galle'},
-];
-console.table(customerData);
