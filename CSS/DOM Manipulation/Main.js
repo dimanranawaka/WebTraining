79820335690660a3ea64c5ff21105eaf2837280e
dimanranawaka <dimanranawaka@gmail.com>
2023-06-26 20:52:08 +0530

    Basic JS - DOM Manipulation

--- CSS/DOM Manipulation/Main.js
+++ CSS/DOM Manipulation/Main.js
@@ -1,6 +1,7 @@
 let myList = document.getElementById('my-list');
 document.getElementById('bnt-add').addEventListener('click',()=>{
     let item = document.createElement('li');
+    item.textContent='Element';
     item.style.backgroundColor='red';
     myList.appendChild(item);
 })
