6393bae9b44b23aede970306ae6205656b37aeda
dimanranawaka <dimanranawaka@gmail.com>
2023-06-26 14:06:30 +0530

    Basic JS - Object(Literal Syntax)

--- CSS/JavaScript/src/Basics.js
+++ CSS/JavaScript/src/Basics.js
@@ -1,3 +1,23 @@
-let cart =[1,2,3];
-let selectedData = cart.indexOf(1);
-console.log(selectedData);
+//=======OOP========
+//Literal Syntax , Class Base(ES6) , Constructor Functions
+//=========================
+
+let man = {
+    nic:90,
+    name:'Diman',
+    age:26,
+    fav:['Games','Coding','Traveling'],
+    friendData:{
+        nic:90,
+        name:'Dasun',
+        age:20,
+        fav:['Games','Coding','Traveling'],
+    },
+    doIt:function () {
+        console.log('Fires');
+    }
+};
+console.log(man);
+console.log(man.age);
+console.log(man.doIt());
+console.log(man.friendData);
