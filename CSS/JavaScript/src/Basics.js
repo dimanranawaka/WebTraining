378ae809885ba6460e15cba493f60021260bd2c1
dimanranawaka <dimanranawaka@gmail.com>
2023-06-26 13:38:18 +0530

    Basic JS - Arrays(find())

--- CSS/JavaScript/src/Basics.js
+++ CSS/JavaScript/src/Basics.js
@@ -1,3 +1,8 @@
 let cart =['Apple','Mango','Banana'];
-let selectedData= cart.find((e)=>e==='Apple');
+// let selectedData= cart.find((e)=>e==='Apple');// Arrow Function
+// console.log(selectedData);
+
+let selectedData = cart.find(function (e) {
+    return e==='Apple';
+})
 console.log(selectedData);
