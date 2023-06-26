2e599e6298124b45374dd96505d755620594f8a4
dimanranawaka <dimanranawaka@gmail.com>
2023-06-26 20:50:30 +0530

    Basic JS - DOM Manipulation

--- CSS/DOM Manipulation/Main.js
+++ CSS/DOM Manipulation/Main.js
@@ -1,23 +1,6 @@
-
-//let divEl1 = document.getElementsByTagName('div')[0];
-let divEl1 = document.getElementById('my-div');
-divEl1.addEventListener('mouseover',()=>{
-    divEl1.style.backgroundColor='green';
-});
-
-divEl1.addEventListener('mouseout',()=>{
-    divEl1.style.backgroundColor='red';
-});
-
-divEl1.addEventListener('mousemove',(e)=>{
-    let x=e.clientX;
-    let y=e.clientY;
-    console.log('X :'+x);
-    console.log('Y :'+y);
-});
-window.addEventListener('mousemove',(e)=>{
-    let x=e.clientX;
-    let y=e.clientY;
-    console.log('X :'+x);
-    console.log('Y :'+y);
-});
+let myList = document.getElementById('my-list');
+document.getElementById('bnt-add').addEventListener('click',()=>{
+    let item = document.createElement('li');
+    item.style.backgroundColor='red';
+    myList.appendChild(item);
+})
