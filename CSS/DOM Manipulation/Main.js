4c8467aeb5cd715206266c71185685c0e46ee124
dimanranawaka <dimanranawaka@gmail.com>
2023-06-26 19:48:48 +0530

    Basic JS - DOM Manipulation

--- CSS/DOM Manipulation/Main.js
+++ CSS/DOM Manipulation/Main.js
@@ -1,10 +1,6 @@
 
 //let divEl1 = document.getElementsByTagName('div')[0];
-let divEl1 = document.getElementById('color-change');
-function change(){
-   if(divEl1.style.display==='none'){
-       divEl1.style.display='block';
-   }else{
-       divEl1.style.display='none';
-   }
-}
+let divEl1 = document.getElementById('my-div');
+divEl1.addEventListener('mouseover',()=>{
+    divEl1.style.backgroundColor='green';
+})
