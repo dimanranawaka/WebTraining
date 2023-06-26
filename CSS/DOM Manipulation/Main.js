9e57a9aacd960ad70e8c1b630899cc4ffe27ce11
dimanranawaka <dimanranawaka@gmail.com>
2023-06-26 15:38:54 +0530

    Basic JS - DOM Manipulation

---
+++ CSS/DOM Manipulation/Main.js
@@ -0,0 +1,8 @@
+let form = document.getElementById('student-form');
+form.addEventListener('submit',function (event) {
+    event.preventDefault();
+    let nameEl = form.elements['fullName'];
+    let emailEl = form.elements['email'];
+    console.log("Full Name : "+nameEl.value);
+    console.log("email : "+emailEl.value);
+})
