7ef4079009707775203a5daa7e85fdf5f9ba51e2
dimanranawaka <dimanranawaka@gmail.com>
2023-06-26 21:13:44 +0530

    Basic JS - DOM Manipulation

--- CSS/DOM Manipulation/Main.js
+++ CSS/DOM Manipulation/Main.js
@@ -1,15 +1,3 @@
-let myList = document.getElementById('my-list');
-let counter=1;
-document.getElementById('bnt-add').addEventListener('click',()=>{
-    let item = document.createElement('li');
-    item.textContent='Element'+counter;
-    item.style.backgroundColor='white';
-    myList.appendChild(item);
-    counter++;
-});
-document.getElementById('bnt-remove').addEventListener('click',()=>{
-    let tempItem = myList.getElementsByTagName('li');
-    if(tempItem.length>0){
-        myList.removeChild(tempItem[tempItem.length-1]);
-    }
+document.getElementById('input-element').addEventListener('keyup',(e)=>{
+    console.log(e.target.value);
 })
