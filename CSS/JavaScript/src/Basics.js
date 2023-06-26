9f17b05974dd35eda29931ae96195fa6581ba2bf
dimanranawaka <dimanranawaka@gmail.com>
2023-06-26 14:18:18 +0530

    Basic JS - Object(Class Base(ES6))

--- CSS/JavaScript/src/Basics.js
+++ CSS/JavaScript/src/Basics.js
@@ -2,10 +2,10 @@
 //Literal Syntax , Class Base(ES6) , Constructor Functions
 //=========================
 
-function Man(name,address,nic){ //Constructor Based Functions
+class Man{constructor (name,address,nic){ //Class Base(ES6)
     this.name = name;
     this.address = address;
     this.nic = nic;
-};
+}};
 let data = new Man('Diman','Galle',50);
 console.log(data);
