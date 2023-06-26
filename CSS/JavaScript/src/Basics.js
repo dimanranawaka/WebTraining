39f2b7c9df402342645f2fcc11a19904373df027
dimanranawaka <dimanranawaka@gmail.com>
2023-06-26 13:35:59 +0530

    Basic JS - Arrays(find())

--- CSS/JavaScript/src/Basics.js
+++ CSS/JavaScript/src/Basics.js
@@ -1,6 +1,3 @@
-let numbers = [1,2,3,4,5,6,7,8,9,10];
-
-let oddNumbers=numbers.filter(data=>data % 2 !==0);
-
-
-console.log(oddNumbers);
+let cart =['Apple','Mango','Banana'];
+let selectedData= cart.find((e)=>e==='Apple');
+console.log(selectedData);
